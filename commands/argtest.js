c04d9bb8b5440348a42dba56e033a7c286f53160
Hydrogen8 <hackmud_hydrogen@proton.me>
2023-04-23 16:40:30 +0100

    Move arginf.js to argtest.js

--- commands/argtest.js
+++ commands/argtest.js
@@ -1,13 +1,16 @@
-const discord = require("discord.js");
+const client = require("discord.js");
 module.exports = {
-	name: 'arginf',
+	name: 'argtest',
 	description: 'Return info about the arguments passed.',
 	args: true,
 	guildOnly: true,
-	aliases: ['ai'],
+	aliases: ['test'],
 	execute(message, args) {
 		if (args[0] === 'foo') {
 			return message.channel.send('bar');}
+		else if (args[0] === 'ping') {
+			return message.channel.send(`Ping! Took ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+		};
 		message.channel.send(`Arguments: ${args}\nArguments length: ${args.length}`);
 	},
 };
