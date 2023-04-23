a1377051b84f84b5a3e47e130f5582a9a0c382b0
Hydrogen8 <hydrogen@envs.net>
2023-04-23 23:08:25 +0100

    Implement error and tag commands

---
+++ commands/error.js
@@ -0,0 +1,15 @@
+require('dotenv').config({ path: '../errordb.env'});
+module.exports = {
+	name: 'error',
+	description: 'Display information about errors.',
+	args: true,
+	aliases: ['err'],
+	execute(message, args) {
+		if(!process.env[args[0]]) {
+			message.channel.send('Error is not in the database.\nIf you know about this error, please contribute to the error database.');
+		}
+		else {
+			message.channel.send(process.env[args[0]]);
+		}
+	}
+}
---
+++ commands/tag.js
@@ -0,0 +1,15 @@
+require('dotenv').config({ path: '../tagdb.env'});
+module.exports = {
+	name: 'tag',
+	description: 'Print contents of a tag from the tag database.',
+	args: true,
+	aliases: [],
+	execute(message, args) {
+		if(!process.env[args[0]]) {
+			message.channel.send('Tag does not exist');
+		}
+		else {
+			message.channel.send(process.env[args[0]]);
+		}
+	}
+}
