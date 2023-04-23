b630836f11c03a7f146c8e3d9b2ca115bcb35a27
Hydrogen8 <hydrogen@envs.net>
2023-04-23 23:39:19 +0100

    Move dotenv imports

--- commands/tag.js
+++ commands/tag.js
@@ -1,10 +1,10 @@
-require('dotenv').config({ path: '../data/tagdb.env'});
 module.exports = {
 	name: 'tag',
 	description: 'Print contents of a tag from the tag database.',
 	args: true,
 	aliases: [],
 	execute(message, args) {
+		require('dotenv').config({ path: '../data/tagdb.env'});
 		if(!process.env[args[0]]) {
 			message.channel.send('Tag does not exist');
 		}
