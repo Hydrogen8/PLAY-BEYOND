fff5040d862aad6fee761a99ec192201e563a374
Hydrogen8 <hackmud_hydrogen@proton.me>
2023-04-23 16:46:23 +0100

    Fix ping test

--- commands/argtest.js
+++ commands/argtest.js
@@ -1,4 +1,3 @@
-const client = require("discord.js");
 module.exports = {
 	name: 'argtest',
 	description: 'Return info about the arguments passed.',
@@ -9,7 +8,7 @@ module.exports = {
 		if (args[0] === 'foo') {
 			return message.channel.send('bar');}
 		else if (args[0] === 'ping') {
-			return message.channel.send(`Ping! Took ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+			return message.channel.send(`Ping! Took ${Date.now() - message.createdTimestamp}ms.`);
 		};
 		message.channel.send(`Arguments: ${args}\nArguments length: ${args.length}`);
 	},
